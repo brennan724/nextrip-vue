033787cc1f829889cacefab06c50ddbfa4e9fa74
brennan724 <brennansmail@gmail.com>
2021-04-28 19:58:36 -0500

    initial working state, commented out code is old shitty self-made dropdown, now using material UI through vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Welcome to the NexTrip API Knock-off
+    <div class="md-layout" id="selectionArea">
+      <dropdown
+        class="md-layout-item"
+        :dropdownData="routeData"
+        @clicked="setRoute"
+      />
+      <dropdown
+        class="md-layout-item"
+        :dropdownData="directionData"
+        @clicked="setDirection"
+      />
+      <dropdown
+        class="md-layout-item"
+        :dropdownData="stopData"
+        @clicked="setStop"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import dropdown from "./components/dropdown.vue";
+import axios from "axios";
 
+const url = `https://svc.metrotransit.org/nextripv2`;
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { dropdown },
+  name: "App",
+  data: function () {
+    return {
+      routeData: [],
+      route: "",
+      directionData: [],
+      direction: "",
+      stopData: [],
+      stop: "",
+      displayInfo: {},
+    };
+  },
+  methods: {
+    setRoute(route_id) {
+      this.route = route_id;
+      console.log(this.route);
+      this.getDirections();
+    },
+    setDirection(direction_id) {
+      this.direction = direction_id;
+      console.log(this.direction);
+      this.getStops();
+    },
+    setStop(stop_id) {
+      this.stop = stop_id;
+      console.log(this.stop);
+      this.getRouteStopInfo();
+    },
+    getRoute() {
+      axios
+        .get(`${url}/routes`)
+        .then((response) => {
+          this.routeData = response.data.map((route) => {
+            return { id: route.route_id, label: route.route_label };
+          });
+          console.log(this.routeData);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getDirections() {
+      axios.get(`${url}/directions/${this.route}`).then((response) => {
+        this.directionData = response.data.map((direction) => {
+          return {
+            id: direction.direction_id,
+            label: direction.direction_name,
+          };
+        });
+        console.log(this.directionData);
+      });
+    },
+    getStops() {
+      axios
+        .get(`${url}/stops/${this.route}/${this.direction}`)
+        .then((response) => {
+          this.stopData = response.data.map((stop) => {
+            return { id: stop.place_code, label: stop.description };
+          });
+          console.log(this.stopData);
+        });
+    },
+    getRouteStopInfo() {
+      axios
+        .get(`${url}/${this.route}/${this.direction}/${this.stop}`)
+        .then((response) => {
+          console.log(response.data);
+          this.displayInfo = response.data;
+        });
+    },
+  },
+  mounted() {
+    this.getRoute();
+  },
+};
 </script>
 
 <style>
---
+++ src/components/dropdown.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="dropdown">
+    <md-field class="dropdownButton">
+      <md-select v-model="selected">
+        <md-option
+          v-for="item in populateDropdown"
+          :key="item.id"
+          :value="item.id"
+        >
+          {{ item.label }}
+        </md-option>
+      </md-select>
+    </md-field>
+    <!-- <md-button class="dropdownButton" @click="toggle">
+      {{ selected || "Choose a value" }}
+    </md-button>
+    <div class="dropdownValues" v-if="active">
+      <div
+        class="dropdownValue"
+        v-for="item in populateDropdown"
+        :key="item.id"
+        @click="setDropdown(item)"
+      >
+        {{ item.label }}
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+  props: ["dropdownData"],
+  data() {
+    return {
+      active: false,
+      selected: "",
+    };
+  },
+  computed: {
+    populateDropdown() {
+      return this.dropdownData;
+    },
+  },
+  methods: {
+    toggle() {
+      this.active = !this.active;
+    },
+    setDropdown(item) {
+      this.selected = item.label;
+      this.$emit("clicked", item.id);
+      this.toggle();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dropdownButton {
+  border: 2px solid black;
+  width: 20em;
+  position: float;
+}
+.dropdownValues {
+  border: 2px solid black;
+  width: 10em;
+  position: float;
+}
+.dropdownValue:hover {
+  background: lightgray;
+}
+</style>
